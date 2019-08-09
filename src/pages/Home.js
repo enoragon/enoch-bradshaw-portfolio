7ad727c5b376043a44cb235c936b823d3540cd0a
Nucky <enoragon@gmail.com>
2019-08-08 23:20:05 -0500

    Stub out home page design

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
-import { Container, Row, Col, Image, } from 'react-bootstrap'
+import { Container, Row, Col, Image, Card } from 'react-bootstrap'
 
 class Home extends Component{
     render(){
         return (
             <Container>
-                <Row className="justify-content-md-center">
+                <Row className="justify-content-md-center mt-5">
                     <Col xs sm="4" md="3" className="d-flex justify-content-sm-end">
                         <Image src="https://via.placeholder.com/150" roundedCircle/>
                     </Col>
@@ -19,6 +19,36 @@ class Home extends Component{
                             </Row>
                         </div>
                     </Col>
+                </Row>
+                <Row className="mt-5 justify-content-md-center">
+                    <Card style={{ width: '70%' }} bg="ghost" variant="dark">
+                        <Card.Body>
+                            <Card.Title>Shit about this dude</Card.Title>
+                            <Card.Text>
+                                Blurbs and other shit about the piece of human trash that is me.
+                            </Card.Text>
+                        </Card.Body>
+                    </Card>
+                </Row>
+                <Row className="mt-5 justify-content-md-center">
+                    <Card style={{ width: '70%' }} bg="ghost" variant="dark">
+                        <Card.Body>
+                            <Card.Title>More shit about this fucker</Card.Title>
+                            <Card.Text>
+                                Blurbs and other shit about the piece of human trash that is me.
+                            </Card.Text>
+                        </Card.Body>
+                    </Card>
+                </Row>
+                <Row className="mt-5 justify-content-md-center">
+                    <Card style={{ width: '70%' }} bg="ghost" variant="dark">
+                        <Card.Body>
+                            <Card.Title>Even more shit about this stupis ass bitch</Card.Title>
+                            <Card.Text>
+                                Blurbs and other shit about the piece of human trash that is me.
+                            </Card.Text>
+                        </Card.Body>
+                    </Card>
                 </Row>
             </Container>
         );
