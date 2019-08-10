349696602a4c47a16d88643e8d18483ab9f643bb
Nucky <enoragon@gmail.com>
2019-08-10 14:21:24 -0500

    Stub out the about page

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,11 +1,33 @@
 import React, { Component } from 'react';
-import { Container } from 'react-bootstrap';
+import { Container, Row, Col, Image } from 'react-bootstrap';
 
 class Home extends Component{
     render(){
         return (
             <Container>
-                
+                <div>
+                    <h1 className="text-center">Meet the team</h1>
+                    <Row>
+                        <Col md="4" className="d-flex justify-content-center offset-md-2">
+                            <Image src="https://via.placeholder.com/150" roundedCircle/>
+                        </Col>
+                        <Col md="4" className="d-flex justify-content-center">
+                            <Image src="https://via.placeholder.com/150" roundedCircle/>
+                        </Col>
+
+                    </Row>
+                    <Row>
+                        <Col md="4" className="d-flex justify-content-center">
+                            <Image src="https://via.placeholder.com/150" roundedCircle/>
+                        </Col>
+                        <Col md="4" className="d-flex justify-content-center">
+                            <Image src="https://via.placeholder.com/150" roundedCircle/>
+                        </Col>
+                        <Col md="4" className="d-flex justify-content-center">
+                            <Image src="https://via.placeholder.com/150" roundedCircle/>
+                        </Col>
+                    </Row>
+                </div>
             </Container>
         );
     }
